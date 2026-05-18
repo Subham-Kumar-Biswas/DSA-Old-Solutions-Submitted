cf705653d0a934b851d2aafcaa48b8ac79a77f42
Subham-Kumar-Biswas <subhamkb.techie@gmail.com>
2026-05-18 21:51:37 +0530

    Day 7 solutions added

---
+++ 07-sorting/07_Squares_of_a_Sorted_Array.java
@@ -0,0 +1,33 @@
+// Leetcode - #977 Squares of a Sorted Array
+// Input: nums = [-4,-1,0,3,10]
+// Output: [0,1,9,16,100]
+
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String[] args) {
+        int[] nums = {-4,-1,0,3,10};
+        System.out.println(Arrays.toString(sortedSquares(nums)));
+    }
+
+    public static int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int left = 0;
+        int right = n - 1;
+        for(int i = n - 1; i >= 0; i--) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if(leftSquare > rightSquare) {
+                result[i] = leftSquare;
+                left++;
+            } else {
+                result[i] = rightSquare;
+                right--;
+            }
+        }
+        return result;
+    }
+}
