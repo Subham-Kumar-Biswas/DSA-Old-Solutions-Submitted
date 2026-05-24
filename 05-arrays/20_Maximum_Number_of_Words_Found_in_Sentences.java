f08249b3f5bbffab22a20bdb897f8f1f43a542d2
Subham-Kumar-Biswas <subhamkb.techie@gmail.com>
2026-05-24 21:58:57 +0530

    Day 5 solutions added

--- 05-arrays/20_Maximum_Number_of_Words_Found_in_Sentences.java
+++ 05-arrays/20_Maximum_Number_of_Words_Found_in_Sentences.java
@@ -2,7 +2,7 @@
 // Input: sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
 // Output: 6
 
-class Solution {
+class MostWordsFound {
     public static void main(String[] args) {
         String[] sentences = {"alice and bob love leetcode", "i think so too", "this is great thanks very much"};
         System.out.println(mostWordsFound(sentences));
