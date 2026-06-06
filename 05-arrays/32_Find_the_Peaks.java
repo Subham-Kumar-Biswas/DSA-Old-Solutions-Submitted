2cdd1b288334909f2239c3e70e4556b9c7a72623
Subham-Kumar-Biswas <subhamkb.techie@gmail.com>
2026-06-06 21:56:23 +0530

    Day 5 solutions added

--- 05-arrays/32_Find_the_Peaks.java
+++ 05-arrays/32_Find_the_Peaks.java
@@ -5,7 +5,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class Solution {
+class FindPeaks {
     public static void main(String[] args) {
         int[] mountain = {1,4,3,8,5};
         System.out.println(findPeaks(mountain));
