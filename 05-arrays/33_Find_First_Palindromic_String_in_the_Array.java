e11292918fef45fd273de2af971917197eed7155
Subham-Kumar-Biswas <subhamkb.techie@gmail.com>
2026-06-07 21:46:07 +0530

    Day 5 solutions added

--- 05-arrays/33_Find_First_Palindromic_String_in_the_Array.java
+++ 05-arrays/33_Find_First_Palindromic_String_in_the_Array.java
@@ -2,7 +2,7 @@
 // Input: words = ["abc","car","ada","racecar","cool"]
 // Output: "ada"
 
-class Solution {
+class FirstPalindrome {
     public static void main(String[] args) {
         String[] words = {"abc","car","ada","racecar","cool"};
         System.out.println(firstPalindrome(words));
