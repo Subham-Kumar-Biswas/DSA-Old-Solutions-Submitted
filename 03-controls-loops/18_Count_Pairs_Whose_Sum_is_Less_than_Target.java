83c46560fa0e771bae4b733b5a4693a924503faf
Subham-Kumar-Biswas <subhamkb.techie@gmail.com>
2026-06-08 21:11:53 +0530

    Day 3 solutions added

---
+++ 03-controls-loops/18_Count_Pairs_Whose_Sum_is_Less_than_Target.java
@@ -0,0 +1,28 @@
+// Leetcode - #2824 Count Pairs Whose Sum is Less than Target
+// Input: nums = [-1,1,2,3,1], target = 2
+// Output: 3
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>(Arrays.asList(-1,1,2,3,1));
+        System.out.println(countPairs(nums, 0));
+    }
+
+    public static int countPairs(List<Integer> nums, int target) {
+        int count = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = i + 1; j < nums.size(); j++) {
+                if ((nums.get(i) + nums.get(j)) < target) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
