671020287343f450347f8d536e92943f21f9102e
Subham-Kumar-Biswas <subhamkb.techie@gmail.com>
2026-06-10 21:23:49 +0530

    Day 8 solutions added

---
+++ 08-strings/14_Truncate_Sentence.java
@@ -0,0 +1,25 @@
+// Leetcode - #1816 Truncate Sentence
+// Input: s = "Hello how are you Contestant", k = 4
+// Output: "Hello how are you"
+
+class Solution {
+    public static void main(String[] args) {
+        String s = "Hello how are you Contestant";
+        int k = 4;
+        System.out.println(truncateSentence(s, k));
+    }
+
+    public static String truncateSentence(String s, int k) {
+        String[] words = s.split(" ");
+
+        StringBuilder truncate = new StringBuilder();
+        for(int i = 0; i < k; i++) {
+            if (i > 0) {
+                truncate.append(" ");
+            }
+            truncate.append(words[i]);
+        }
+
+        return truncate.toString();
+    }
+}
