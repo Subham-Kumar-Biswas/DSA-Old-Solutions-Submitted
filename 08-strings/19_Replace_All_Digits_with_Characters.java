306becd6b69bf28b85d95037d1cc709774df2577
Subham-Kumar-Biswas <subhamkb.techie@gmail.com>
2026-06-17 23:22:55 +0530

    Day 8 solutions added

---
+++ 08-strings/19_Replace_All_Digits_with_Characters.java
@@ -0,0 +1,23 @@
+// Leetcode - #1844 Replace All Digits with Characters
+// Input: s = "a1c1e1"
+// Output: "abcdef"
+
+class Solution {
+    public static void main(String[] args) {
+        String s = "a1c1e1";
+        System.out.println(replaceDigits(s));
+    }
+
+    public static String replaceDigits(String s) {
+        char[] arr = s.toCharArray();
+
+        for (int i = 1; i < arr.length; i += 2) {
+            char previousLetter = arr[i - 1];
+            int shiftAmount = arr[i] - '0';
+
+            arr[i] = (char) (previousLetter + shiftAmount);
+        }
+
+        return new String(arr);
+    }
+}
